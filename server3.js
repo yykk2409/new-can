1b5758859b037d6cc6a8987bd3d89dc495fb2a74
yykk2409 <132257459+yykk2409@users.noreply.github.com>
2024-04-20 17:38:11 +0900

    Update server3.js

--- server3.js
+++ server3.js
@@ -12,7 +12,7 @@ dotenv.config();
 const pool = new Pool({
   user: process.env.POSTGRES_USER,
   host: process.env.POSTGRES_HOST,
-  database: process.env.OSTGRES_DATABASE,
+  database: process.env.POSTGRES_DATABASE,
   password: process.env.POSTGRES_PASSWORD,
   port: 5432, // PostgreSQLのデフォルトポート
 　ssl: {
